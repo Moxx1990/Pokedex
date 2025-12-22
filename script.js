735f269e455e336b73064afef189112ba950b2a1
Moxx1990 <schmidt-max@gmx.de>
2025-12-22 09:58:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let pokemonloadIndex = 20;
+
+
+async function getPokemonStart() {
+    
+}
+
+async function getPokemonData(i) {
+    
+}
+
+async function pokemonPreview(i) {
+    
+}
+
+function colorPokemonType() {
+    
+}
+
+
+async function createPokemonCard() {
+    
+} 
+
+
+async function loadMorePokemon() {
+    let pokemonloadIndexEnd = pokemonloadIndex + 20;
+}
+
+function searchPokemon() {
+    
+}
+
+function darkModeToggle() {
+    
+}
+
+
