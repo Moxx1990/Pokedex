252820052692fb436c2e7479ba8200ce8dafc202
Moxx1990 <schmidt-max@gmx.de>
2025-12-22 16:18:03 +0100

    Added Bootstrap, onload function, Navbar, Searchbar, Load More Button

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-let pokemonloadIndex = 20;
+let pokemonloadIndex = 10;
+let currentPokemon = [];
 
 
 async function getPokemonStart() {
-    
+    getPokemonData();
 }
 
-async function getPokemonData(i) {
+async function getPokemonData() {
+    let amountOfPokemon = pokemonloadIndex;
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${amountOfPokemon}&offset=0`;
+    let response = await fetch(url);
+    let loadedPokemon = await response.json();  
+
+     let pokemonDetails = await Promise.all(
+        loadedPokemon.results.map(async (pokemon) => {
+            let res = await fetch(pokemon.url);
+            return await res.json();
+        })
+    );
+
+    currentPokemon = pokemonDetails;
+    console.log(loadedPokemon);    
+    console.log(pokemonDetails);
     
+    pokemonPreview();  
+
 }
 
-async function pokemonPreview(i) {
-    
-}
+async function pokemonPreview() {
+    let container = document.getElementById('pokemon-container');
+    container.innerHTML = '';
+    for (let index = 0; index < currentPokemon.length; index++) {
+        let pokemonName = currentPokemon[index].name;
+        let pokemonId= currentPokemon[index].id;
+        let pokemonImage = currentPokemon[index].sprites.front_default;
+        let pokemonType = currentPokemon[index].types[0].type.name;
 
-function colorPokemonType() {
-    
-}
+       container.innerHTML += `<div class="pokemon-card" id="pokemon-card-${index}" onclick="createPokemonCard(${index})">
+            <div>#${pokemonId} ${pokemonName}</div>
+            <div class="${pokemonType}"><img src="${pokemonImage}"> </div>
+            <div><img src="./picture/${pokemonType}.png" class="${pokemonType} pokemon-type-icon"></div>
+    </div>`;
+} 
+        
+    }    
 
+async function createPokemonCard(index) {
 
-async function createPokemonCard() {
-    
 } 
 
 
 async function loadMorePokemon() {
-    let pokemonloadIndexEnd = pokemonloadIndex + 20;
+    pokemonloadIndex = pokemonloadIndex + 10;
+    getPokemonStart();
 }
 
 function searchPokemon() {
