44a94ce5d6ce00f5439d0861172ed5f084c3a1e9
Moxx1990 <schmidt-max@gmx.de>
2026-01-12 11:26:46 +0100

    added close Button, error message and maximum content, console error fixed

--- script.js
+++ script.js
@@ -49,7 +49,14 @@ async function pokemonPreview() {
         let pokemonType = currentPokemon[index].types[0].type.name;
         let pokemonSecondType = currentPokemon[index].types[1]?.type.name || "none";
 
-        container.innerHTML += 
+       createPreviewCard(pokemonName, pokemonId, pokemonImage, pokemonType, pokemonSecondType, index);
+    } 
+}
+
+function createPreviewCard(pokemonName, pokemonId, pokemonImage, pokemonType, pokemonSecondType, index) {
+    let container = document.getElementById('pokemon-container');
+
+    container.innerHTML += 
         `<div class="pokemon-card" id="pokemon-card-${index}" onclick="createPokemonCard(${index})">
             <div>#${pokemonId} ${pokemonName}</div>
             <div class="${pokemonType} pokemon-card-image"><img src="${pokemonImage}"> </div>
@@ -58,14 +65,14 @@ async function pokemonPreview() {
                 <img src="./picture/${pokemonSecondType}.png" class="${pokemonSecondType} pokemon-type-icon" onerror="this.style.display='none'">
             </div>
         </div>`;
-    } 
 }
 
 function createPokemonCard(index) {
     document.body.style.overflow = 'hidden';
     event.stopPropagation();
     showPokemonCard();
-    loadPokemonCardStructure(index);  
+    loadPokemonCardStructure(index); 
+    loadPokemonInfo(index); 
     loadTypeNavigation(index); 
     loadPokemonPicture(index);
     loadPokemonStats(index);
@@ -84,22 +91,27 @@ function loadPokemonCardStructure(index) {
 
     loadedCardContainer.innerHTML = `
         <div class="loaded_card" id="loaded-card-${index}">
+            <div class="close-button" onclick="closePokemonCard()">&#10006;</div>
             <div class="pokemon-picture" id="pokemon-picture"></div>
-            <div class="pokemon-info" id="pokemon-info">
-                <h2 class="pokemon-card-name" id="pokemon-card-name">${pokemon.name}</h2>
-                <div class="pokemon-stats" id="pokemon-stats"></div>
-                <div class="pokemon-attacks" id="pokemon-attacks"></div>
-            </div>
+            <div class="pokemon-info" id="pokemon-info"></div>
             <div class="pokemon-type-navigation" id="pokemon-type-navigation"></div>
         </div>`;
 }
 
-function loadTypeNavigation(index) {
+function loadPokemonInfo(index) {
     let pokemon = currentPokemon[index];
+    document.getElementById('pokemon-info').innerHTML = `
+        <h2 class="pokemon-card-name" id="pokemon-card-name">${pokemon.name}</h2>
+                <div class="pokemon-stats" id="pokemon-stats"></div>
+                <div class="pokemon-attacks" id="pokemon-attacks"></div>`;
+}
+
+function loadTypeNavigation(index) {
+    
     document.getElementById('pokemon-type-navigation').innerHTML = `
-        <div class="backward" onclick="previousPokemonCard(${index})"> <--- </div>
+        <div class="backward" onclick="previousPokemonCard(${index})"> &#129032 </div>
         <div class="pokemon-type" id="pokemon-type"></div>
-        <div class="forward" onclick="nextPokemonCard(${index})"> ---> </div> `;
+        <div class="forward" onclick="nextPokemonCard(${index})"> &#129034 </div> `;
 }
 
 function loadPokemonPicture(index) {
@@ -114,12 +126,12 @@ function loadPokemonStats(index) {
     
     document.getElementById('pokemon-stats').innerHTML = '';
     document.getElementById('pokemon-stats').innerHTML = `
-        <div class="hp-stat progress-bar"><div class="bar" style="--value:${pokemon.stats[0].base_stat}">${pokemon.stats[0].base_stat}</div></div></div>
-        <div class="attack-stat progress-bar"><div class="bar" style="--value:${pokemon.stats[1].base_stat}">${pokemon.stats[1].base_stat}</div></div>
-        <div class="defense-stat progress-bar"><div class="bar" style="--value:${pokemon.stats[2].base_stat}">${pokemon.stats[2].base_stat}</div></div>
-        <div class="special-attack-stat progress-bar"><div class="bar" style="--value:${pokemon.stats[3].base_stat}">${pokemon.stats[3].base_stat}</div></div>
-        <div class="special-defense-stat progress-bar"><div class="bar" style="--value:${pokemon.stats[4].base_stat}">${pokemon.stats[4].base_stat}</div></div>
-        <div class="speed-stat progress-bar"><div class="bar" style="--value:${pokemon.stats[5].base_stat}">${pokemon.stats[5].base_stat}</div></div>`;
+        <div class="hp-stat progress-bar"><div class="bar ${pokemon.types[0].type.name}" style="--value:${pokemon.stats[0].base_stat}">${pokemon.stats[0].base_stat}</div></div></div>
+        <div class="attack-stat progress-bar"><div class="bar ${pokemon.types[0].type.name}" style="--value:${pokemon.stats[1].base_stat}">${pokemon.stats[1].base_stat}</div></div>
+        <div class="defense-stat progress-bar"><div class="bar ${pokemon.types[0].type.name}" style="--value:${pokemon.stats[2].base_stat}">${pokemon.stats[2].base_stat}</div></div>
+        <div class="special-attack-stat progress-bar"><div class="bar ${pokemon.types[0].type.name}" style="--value:${pokemon.stats[3].base_stat}">${pokemon.stats[3].base_stat}</div></div>
+        <div class="special-defense-stat progress-bar"><div class="bar ${pokemon.types[0].type.name}" style="--value:${pokemon.stats[4].base_stat}">${pokemon.stats[4].base_stat}</div></div>
+        <div class="speed-stat progress-bar"><div class="bar ${pokemon.types[0].type.name}" style="--value:${pokemon.stats[5].base_stat}">${pokemon.stats[5].base_stat}</div></div>`;
 }
 
 function loadPokemonAttacks(index) {
@@ -144,13 +156,56 @@ function loadPokemonTypes(index) {
 }
 
 function nextPokemonCard(index) {
-    currentPokemon[index ++];
+    if (!checkNextPokemonIndex(index)) {
+        createPokemonCard(index);
+        return;
+    }
+    index ++;
     createPokemonCard(index);
 }
 
 function previousPokemonCard(index) {
-    currentPokemon[index --];
+    if (!checkPreviousPokemonIndex(index)) {
+        createPokemonCard(index);
+        return;
+    }
+    index--;
     createPokemonCard(index);
+}
+
+function checkPreviousPokemonIndex(index) {
+   let errorDiv = document.getElementById('error-message');
+    errorDiv.innerHTML = '';
+
+    if (index <= 0) {
+        showError('You are at the first Pokemon');
+        return false; // ungültiger Index
+    }
+   
+    return true; // Index ist gültig
+}
+
+function checkNextPokemonIndex(index) {
+    let errorDiv = document.getElementById('error-message');
+    errorDiv.innerHTML = '';
+
+     if (index >= currentPokemon.length - 1) {
+        showError('Please load more Pokemon');
+        return false;
+    }
+    return true;
+}
+
+function showError (message) {
+    let modal = document.getElementById('error-modal');
+    let messageP = document.getElementById('error-message');
+
+    messageP.innerText = message;
+    modal.style.display = 'flex';
+
+    setTimeout (() => {
+        modal.style.display = 'none';
+    }, 1000);
 }
 
 document.addEventListener('click', (event) => {
@@ -177,14 +232,15 @@ async function loadMorePokemon() {
 
 function searchPokemon() {
     let input = getSearchInput();
+    if (input.length < 3 && isNaN(input)) {
+        return;
+    }
     let container = clearContainer();
     let results = getMatchingPokemon(input);
-
     if (results.length === 0) {
         showNoResults(container);
         return;
     }
-
     results.forEach(pokemon => renderPokemonCard(pokemon, container));
 }
 
@@ -209,12 +265,16 @@ function renderPokemonCard(pokemon, container) {
     let index = currentPokemon.indexOf(pokemon);
     let type1 = pokemon.types[0].type.name;
     let type2 = pokemon.types[1]?.type.name || "none";
+    
+    renderSearchCard(index, type1, type2, container);
+}
 
+function renderSearchCard(index, type1, type2, container) {
     container.innerHTML += `
         <div class="pokemon-card" onclick="createPokemonCard(${index})">
-            <div>#${pokemon.id} ${pokemon.name}</div>
+            <div>#${currentPokemon[index].id} ${currentPokemon[index].name}</div>
             <div class="${type1} pokemon-card-image">
-                <img src="${pokemon.sprites.other.home.front_default}">
+                <img src="${currentPokemon[index].sprites.other.home.front_default}">
             </div>
             <div class="pokemon-card-type">
                 <img src="./picture/${type1}.png" class="${type1} pokemon-type-icon">
